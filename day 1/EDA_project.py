851afb1a36449e66ad5f3d6a361faa6bfe884610
vaibhavvm2005 <vaibhavmadival331@gmail.com>
2026-06-26 14:15:02 +0530

    Initial commit

---
+++ day 1/EDA_project.py
@@ -0,0 +1,69 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns   
+import os
+
+print("understanding the dataset")
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+file_name = os.path.join(script_dir, 'sales_data.csv')
+if not os.path.exists(file_name):
+    print(f"error:{file_name} is not found:")
+    exit()
+
+
+df=pd.read_csv(file_name)
+print("succesfully loaded")
+print(f"Shape pf the dataset:Rows{ df.shape[0]},columns:{df.shape[1]}")
+
+
+print(df.head())
+print(df.tail())
+print(df.shape)
+print(df.describe())
+
+
+median_age=df['Age'].median()
+df['Age']=df['Age'].fillna(median_age)
+print(median_age)
+
+median_spending=df["spending"].median()
+df["spending"]=df["spending"].fillna(median_spending)
+print(median_spending)
+
+mean_age=df['Age'].mean()
+df['Age']=df['Age'].fillna(mean_age)
+print(mean_age)
+
+mean_spending=df["spending"].mean()
+df["spending"]=df["spending"].fillna(mean_spending)
+print(mean_spending)
+
+
+plt.figure(figsize=(12,8))
+df["spending"].hist(bins=10,color="skyblue",edgecolor="black")
+plt.title("distribution of spending")
+plt.xlabel("spending amount")
+plt.ylabel("Number of Customers")
+plt.show()
+
+correlation=df.corr(numeric_only=True)
+print(correlation)
+
+print("Plotting Coorelation Hatmap")
+plt.figure(figsize=(7,4))
+sns.heatmap(correlation,annot=True,cmap="coolwarm",fmt=".5f")
+plt.title("coorelation Heatmap")
+plt.show()
+
+
+#plt.figure(figsize=(7,4))
+#sns.boxplot(x=df['Age'],color='lightgreen')
+#plt.title("Boxplot of customer age")
+#plt.xlabel('Age')
+#plt.show()
+
+print("find the outliers in age")
+outliers=df[df["Age"]>100]
+print("find the outliers(s):")
+print(outliers)
